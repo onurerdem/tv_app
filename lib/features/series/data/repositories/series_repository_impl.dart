bfd868f8872955ffa3d854d585c68384e9d4e8dc
Onur Erdem <erdem.onur@outlook.com.tr>
2024-11-10 17:22:11 +0300

    Repository and Implementation added.

---
+++ lib/features/series/data/repositories/series_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../../domain/entities/series.dart';
+import '../../domain/repositories/series_repository.dart';
+import '../datasources/series_remote_data_source.dart';
+
+class SeriesRepositoryImpl implements SeriesRepository {
+  final SeriesRemoteDataSource remoteDataSource;
+
+  SeriesRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failure, List<Series>>> getAllSeries() async {
+    try {
+      final series = await remoteDataSource.getAllSeries();
+      return Right(series);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Series>>> searchSeries(String query) async {
+    try {
+      final series = await remoteDataSource.searchSeries(query);
+      return Right(series);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+}
---
+++ lib/features/series/domain/repositories/series_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../entities/series.dart';
+
+abstract class SeriesRepository {
+  Future<Either<Failure, List<Series>>> getAllSeries();
+  Future<Either<Failure, List<Series>>> searchSeries(String query);
+}
