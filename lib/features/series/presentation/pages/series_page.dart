fa77f8d23990749df022e154dbab503509e42032
Onur Erdem <erdem.onur@outlook.com.tr>
2024-11-14 23:55:27 +0300

    SeriesPage fixed.

--- lib/features/series/presentation/pages/series_page.dart
+++ lib/features/series/presentation/pages/series_page.dart
@@ -21,6 +21,12 @@ class _SeriesPageState extends State<SeriesPage> {
     bloc.add(GetAllSeriesEvent());
   }
 
+  Future<void> _refreshData() async {
+    bloc = context.read<SeriesBloc>();
+    bloc.add(GetAllSeriesEvent());
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
     final TextEditingController _controller = TextEditingController();
@@ -28,107 +34,111 @@ class _SeriesPageState extends State<SeriesPage> {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Series')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              cursorColor: Colors.black,
-              decoration: InputDecoration(
-                labelText: 'Search Series',
-                floatingLabelStyle: TextStyle(
-                  color: Colors.black,
+      body: RefreshIndicator(
+        onRefresh: _refreshData,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _controller,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  labelText: 'Search for series.',
+                  floatingLabelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      bloc.add(
+                        SearchSeriesQuery(_controller.text.trim()),
+                      );
+                      FocusScope.of(context).unfocus();
+                    },
+                  ),
                 ),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    bloc.add(
-                      SearchSeriesQuery(_controller.text.trim()),
-                    );
-                    FocusScope.of(context).unfocus();
+                onChanged: (query) {
+                  bloc.add(
+                    SearchSeriesQuery(query.trim()),
+                  );
+                },
+                textInputAction: TextInputAction.search,
+                onSubmitted: (query) {
+                  bloc.add(
+                    SearchSeriesQuery(query.trim()),
+                  );
+                },
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: BlocBuilder<SeriesBloc, SeriesState>(
+                  builder: (context, state) {
+                    if (state is SeriesLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (state is SeriesLoaded) {
+                      return ListView.separated(
+                        separatorBuilder: (context, index) => const Divider(),
+                        itemCount: state.seriesList.length,
+                        itemBuilder: (context, index) {
+                          final series = state.seriesList[index];
+                          return Row(
+                            children: [
+                              SizedBox(
+                                height: 160,
+                                child: series.imageUrl != null
+                                    ? ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(16.0),
+                                        child: Image.network(
+                                          series.imageUrl!,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      )
+                                    : null,
+                              ),
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      series.name,
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      series.summary != null
+                                          ? "${series.summary?.replaceAll(RegExp(r'<[^>]*>'), '')}"
+                                          : "Explanation not available.",
+                                      style: const TextStyle(fontSize: 12),
+                                      maxLines: 4,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      series.genres.isNotEmpty
+                                          ? series.genres.join(', ')
+                                          : "No species information available.",
+                                      style: const TextStyle(fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else if (state is SeriesError) {
+                      return Center(child: Text(state.message));
+                    }
+                    return const Center(
+                        child: Text("Enter a term to search for a serie."));
                   },
                 ),
               ),
-              onChanged: (query) {
-                bloc.add(
-                  SearchSeriesQuery(query.trim()),
-                );
-              },
-              textInputAction: TextInputAction.search,
-              onSubmitted: (query) {
-                bloc.add(
-                  SearchSeriesQuery(query.trim()),
-                );
-              },
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-              child: BlocBuilder<SeriesBloc, SeriesState>(
-                builder: (context, state) {
-                  if (state is SeriesLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (state is SeriesLoaded) {
-                    return ListView.separated(
-                      separatorBuilder: (context, index) => const Divider(),
-                      itemCount: state.seriesList.length,
-                      itemBuilder: (context, index) {
-                        final series = state.seriesList[index];
-                        return Row(
-                          children: [
-                            SizedBox(
-                              height: 160,
-                              child: series.imageUrl != null
-                                  ? ClipRRect(
-                                      borderRadius: BorderRadius.circular(16.0),
-                                      child: Image.network(
-                                        series.imageUrl!,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    )
-                                  : null,
-                            ),
-                            const SizedBox(width: 16),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    series.name,
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    series.summary != null
-                                        ? "${series.summary?.replaceAll(RegExp(r'<[^>]*>'), '')}"
-                                        : "Explanation not available.",
-                                    style: const TextStyle(fontSize: 12),
-                                    maxLines: 4,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    series.genres.isNotEmpty
-                                        ? series.genres.join(', ')
-                                        : "No species information available.",
-                                    style: const TextStyle(fontSize: 12),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  } else if (state is SeriesError) {
-                    return Center(child: Text(state.message));
-                  }
-                  return const Center(
-                      child: Text("Enter a term to search for a serie."));
-                },
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
