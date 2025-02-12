49facb1af986b78fb32028e831d0195dceef414c
Onur Erdem <erdem.onur@outlook.com.tr>
2025-02-13 01:21:19 +0300

    Repository and Implementation added.

---
+++ lib/features/authentication/data/repositories/firebase_repository_impl.dart
@@ -0,0 +1,34 @@
+import '../../domain/entities/user_entity.dart';
+import '../../domain/repositories/firebase_repository.dart';
+import '../datasources/firebase_remote_data_source.dart';
+
+class FirebaseRepositoryImpl extends FirebaseRepository{
+  final FirebaseRemoteDataSource remoteDataSource;
+
+  FirebaseRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<void> getCreateCurrentUser(UserEntity user) async =>
+      remoteDataSource.getCreateCurrentUser(user);
+
+  @override
+  Future<String> getCurrentUId() async =>
+      remoteDataSource.getCurrentUId();
+
+  @override
+  Future<bool> isSignIn() async =>
+      remoteDataSource.isSignIn();
+
+  @override
+  Future<void> signIn(UserEntity user) async =>
+      remoteDataSource.signIn(user);
+
+  @override
+  Future<void> signOut() async =>
+      remoteDataSource.signOut();
+
+  @override
+  Future<void> signUp(UserEntity user) async =>
+      remoteDataSource.signUp(user);
+
+}
